784e699d9dfad247d8d623d62e148435c3460ece
Ramill <ram4ik@hotmail.com>
2024-10-26 15:19:54 +0300

    Initial Commit

---
+++ CloudSonete/CloudSoneteApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudSoneteApp.swift
+//  CloudSonete
+//
+//  Created by admin on 10/26/24.
+//
+
+import SwiftUI
+
+@main
+struct CloudSoneteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
